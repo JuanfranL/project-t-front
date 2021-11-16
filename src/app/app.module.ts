c409a269d3aed2bc4b9dd4148d288218c1781f8b
Daniel <hayber1995@gmail.com>
2021-11-16 20:31:58 +0100

    nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ComponentePrincipal } from './app.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ComponentePrincipal,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ComponentePrincipal]
 })
 export class AppModule { }
